8f6d03dff207c1a427de95c0bff257cb4d2aea8a
GhulamAli <10416351+ali38241@users.noreply.github.com>
2023-04-10 15:26:07 +0500

    change controller

--- backup/src/main/java/com/data/backup/app/AppController.java
+++ backup/src/main/java/com/data/backup/app/AppController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/backup")
+@RequestMapping("/")
 public class AppController {
 	
 	private AppService appService;
