c8e2e57174f02dc981d27b7c8b420b46b799f77a
eido38990 <64368997+eido38990@users.noreply.github.com>
2023-06-01 14:33:29 +0500

    encryption

--- backup/src/main/java/com/data/backup/BackupApplication.java
+++ backup/src/main/java/com/data/backup/BackupApplication.java
@@ -2,17 +2,20 @@ package com.data.backup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import com.data.backup.app.AppService;
 
 
 @SpringBootApplication
+
 public class BackupApplication {
 
 	public static void main(String[] args) {
-//		AppService appService = new AppService();
-//		appService.intialize();
 		SpringApplication.run(BackupApplication.class, args);
+		
+		
+		
 		
 	}
 
